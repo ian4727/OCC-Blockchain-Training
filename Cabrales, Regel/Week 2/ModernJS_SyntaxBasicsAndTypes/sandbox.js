4561331c0c185a013fd1448f8bb5b1717f561234
regel <regelcabrales@gmail.com>
2023-09-05 02:13:49 +0800

    ModernJS & Calculator App

---
+++ Cabrales, Regel/Week 2/ModernJS_SyntaxBasicsAndTypes/sandbox.js
@@ -0,0 +1,95 @@
+// alert("hello world");
+console.log(1);
+console.log(2);
+
+// VARIABLES
+let age = 25;
+let year = 2023;
+// log things to console
+console.log(age, year);
+age = 30;
+console.log(age);
+
+const point = 100;
+console.log(point);
+
+// old way of creating variables
+var score = 75;
+console.log(score);
+
+// STRINGS
+console.log('hello, world');
+let email = 'regelcabrales@gmail.com'
+console.log(email);
+
+// string concatenation
+let firstName = 'Regel';
+let lastName = 'Cabrales';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting character
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log('index');
+
+// common string methods
+
+// Shows index of certain character 
+// let result2 = email.lastIndexOf('c');
+
+// Slices string, (from, up to) 
+// let result2 = email.slice(0,5);
+
+// similar to slicing but parameter is (start, number of characters)
+// let result2 = email.substring(4, 10);
+
+// replace first instance of character to another
+// let result2 = email.replace('r', 'y');
+
+// console.log(result2);
+
+// NUMBERS
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +. -, *, /, **, %
+
+// modulo
+//  console.log(10/2);
+//  let result3 = radius % 3;
+
+// multiplication and exponent
+// let result3 = pi * radius**2;
+
+// B-Brackets I-Indices D-Division M-Multiplication A-Addition S-Subtraction
+// let result3 = 5 * (10-3)**2;
+// console.log(results3);
+
+// let likes = 10;
+// likes = likes + 1;
+// like++;
+// likes--;
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// like /= 2;
+// console.log(likes);
+
+// NaN = not a number 
+// console.log(5 / 'hello');
+
+// let result4 = 'the blog has ' + likes + ' likes';
+// console.log(result4);
+
