ef45a8a01a57a71227c30eeef4008bf38ca52ff7
jlsarmiento1996 <jlsarmiento1996@gmail.com>
2023-09-06 07:53:03 +0800

    added activities and files

---
+++ John Luigi Sarmiento/Javascript/object.js
@@ -0,0 +1,28 @@
+let ninjas =['shaun','ryu', 'chun-li'];
+ninjas [1] = 'ken';
+console.log(ninjas[1]);
+console.log(ninjas)
+
+// array methods 
+// let result = ninjas.join(',')
+// console.log(result)
+
+// let result = ninjas.indexOf('chun-li')
+// console.log(result)
+
+let result = ninjas.push('ken');
+console.log(ninjas)
+let result1 = ninjas.pop();
+console.log(result1)
+
+let age = null;
+console.log(age, age + 3, `the age is ${age}`)
+
+// booleans & comparison 
+console.log(true, false,"true","false");
+
+let email = 'luigi@thenetninja.co.uk'
+
+let result3 = email.includes('@')
+console.log(result3)
+
