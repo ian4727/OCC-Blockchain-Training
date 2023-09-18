2f6f58b545af3a03e818c4f0275c4d788f4b3f8f
Jared Neil Hortaleza <jaredneilhortaleza11@gmail.com>
2023-09-18 16:45:14 +0800

    edited NFT collections projects

--- Hortaleza, Jared Neil/NFT-Mystery-Game/hardhat/scripts/deploy.js
+++ Hortaleza, Jared Neil/NFT-Mystery-Game/hardhat/scripts/deploy.js
@@ -2,11 +2,12 @@ const { ethers } = require("hardhat");
 // const { TOKEN_ADDRESS } = require("../constants");
 
 async function main() {
-  const LootboxContract = await ethers.deployContract("Lootbox");
-
-  await LootboxContract.waitForDeployment();
-
-  console.log(`Contract deployed to: ${LootboxContract.target}`);
+  // const LootboxContract = await ethers.deployContract("Lootbox");
+  // await LootboxContract.waitForDeployment();
+  // console.log(`Contract deployed to: ${LootboxContract.target}`);
+  const MyTokenContract = await ethers.deployContract("MyToken", ["0xfF089ea6c39403134B6Ad4aDD5087d9c3aa38350"]);
+  await MyTokenContract.waitForDeployment();
+  console.log(`Contract deployed to: ${MyTokenContract.target}`);
 }
 
 main()
--- Hortaleza, Jared Neil/music-mystery-NFT/hardhat/scripts/deploy.js
+++ Hortaleza, Jared Neil/music-mystery-NFT/hardhat/scripts/deploy.js
@@ -1,9 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const MusicMysteryContract = await hre.ethers.deployContract("MusicMysteryNFT");
-  await MusicMysteryContract.waitForDeployment();
-  console.log(`Contract deployed to: ${MusicMysteryContract.target}`);
+  // const MusicMysteryContract = await hre.ethers.deployContract("MusicMysteryNFT");
+  // await MusicMysteryContract.waitForDeployment();
+  // console.log(`Contract deployed to: ${MusicMysteryContract.target}`);
+  const MyTokenContract = await hre.ethers.deployContract("MyToken", ["0x7Bd405b37EB761E0AB5d9cf79972D1a9704e9CBa"]);
+  await MyTokenContract.waitForDeployment();
+  console.log(`Contract deployed to: ${MyTokenContract.target}`);
 }
 
 main()
